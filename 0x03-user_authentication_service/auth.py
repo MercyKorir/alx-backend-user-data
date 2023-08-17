307adc146f1b7a35c852258bc56cfd4c90a07843
MercyKorir <mcthegreat78@gmail.com>
2023-08-17 15:46:52 +0300

    I have added the file for task 4*

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Auth module"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """hashes password"""
+    salt = bcrypt.gensalt()
+    hash_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hash_pwd
