a9ae7154c03c70d3f861df81c85d92b1995760f5
MercyKorir <mcthegreat78@gmail.com>
2023-08-18 12:14:08 +0300

    I have added the get_user_from_session_id method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import uuid
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Optional
 
 
 def _hash_password(password: str) -> bytes:
@@ -56,3 +57,13 @@ class Auth:
         user_uuid = _generate_uuid()
         self._db.update_user(user_id, session_id=user_uuid)
         return user_uuid
+
+    def get_user_from_session_id(self, session_id: str) -> Optional[User]:
+        """Returns user from session_id"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
