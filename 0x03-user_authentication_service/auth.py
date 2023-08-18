175a1af46c16ca14c36af84054fa5b49e497f400
MercyKorir <mcthegreat78@gmail.com>
2023-08-18 10:48:52 +0300

    I have added the file for task 5*

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -18,11 +18,11 @@ class Auth:
     def __init__(self) -> None:
         self._db = DB()
 
-    def register_user(self, email: str, password:str) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """creates new user"""
         try:
             ex_user = self._db.find_user_by(email=email)
-            raise ValueError('Email already registered')
+            raise ValueError('User {} already exists'.format(email))
         except ValueError:
             hash_pwd = _hash_password(password)
             new_user = self._db.add_user(email, hash_pwd)
